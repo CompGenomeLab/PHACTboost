f8c688af0b2055d7b3859e789027938cd0b1a972
Nurdan Kuru <68369488+nurdannkuru@users.noreply.github.com>
2023-09-09 23:09:33 +0300

    Delete Tools with Small Num. of Variants

--- dbNSFP/Combine_VariantSet_and_dbNSFP.R
+++ dbNSFP/Combine_VariantSet_and_dbNSFP.R
@@ -6,7 +6,7 @@ all <- rbind(train, test)
 rm(train, test)
 
 data$chr_var_with_aa <- paste(data$`#chr`, "-", data$`pos(1-based)`, data$ref, ">", data$alt, "-",
-                      data$aaref, data$aaalt, sep = "")
+                              data$aaref, data$aaalt, sep = "")
 all$chr_var_with_aa <- paste(all$chr_vars, "-", all$Ref_AA, all$Alt_AA, sep = "")
 
 common_vars <- intersect(data$chr_var_with_aa, all$chr_var_with_aa)
@@ -21,19 +21,24 @@ dbnsfp_allvar$prot_vars <- all_sub$prot_vars
 dbnsfp_allvar$variant_info <- all_sub$variant_info
 dbnsfp_allvar$UNIPROTKB <- all_sub$UNIPROTKB
 
-tools <- unique(c(grep("score", colnames(dbnsfp_allvar)), grep("pred", colnames(dbnsfp_allvar))))
+tools <- grep("rankscore", colnames(dbnsfp_allvar))
+tools_considering_AF <- c("MetaSVM_rankscore", "MetaLR_rankscore", "MetaRNN_rankscore",
+                          "BayesDel_addAF_rankscore", "BayesDel_noAF_rankscore", "M-CAP_rankscore",
+                          "MutPred_rankscore", "ClinPred_rankscore", "Eigen-raw_coding_rankscore")
+tools_lessnumvar <- c("LINSIGHT_rankscore")
+
+tools <- tools[-match(tools_considering_AF, colnames(dbnsfp_allvar)[tools])]
+tools <- tools[-match(tools_lessnumvar, colnames(dbnsfp_allvar)[tools])]
+
 dbnsfp_allvar <- dbnsfp_allvar[, c(1:13, tools, 14:37, 786:789)]
 
 save(dbnsfp_allvar, file="dbNSFP_44a_CompleteSet.RData")
 
-load("/Users/nurdankuru/Desktop/PHACTboost_Final/Test_gnomAD_Shared.RData")
+load("Test_gnomAD_Shared.RData")
 
 common_vars_test <- intersect(test$prot_vars, dbnsfp_allvar$prot_vars)
 dbnsfp_testset <- dbnsfp_allvar[match(common_vars_test, dbnsfp_allvar$prot_vars),]
 
 save(dbnsfp_testset, file="dbNSFP_44a_TestSet.RData")
-
-
-
 
 
