9ab5db5ba6eb08be35a92565f2c77dc815028af6
Nurdan Kuru <68369488+nurdannkuru@users.noreply.github.com>
2023-09-09 23:22:53 +0300

    Update Combine_VariantSet_and_dbNSFP.R

--- dbNSFP/Combine_VariantSet_and_dbNSFP.R
+++ dbNSFP/Combine_VariantSet_and_dbNSFP.R
@@ -1,44 +1,42 @@
+load("/Users/nurdankuru/Desktop/dbNSFP_43a_AllVariantSet_AllColumns.RData")
+load("/Users/nurdankuru/Desktop/PHACTboost/Train_gnomAD_Shared.RData")
+load("/Users/nurdankuru/Desktop/PHACTboost/Test_gnomAD_Shared.RData")
+
+all <- rbind(train, test)
+rm(train, test)
+
+data$chr_var_with_aa <- paste(data$`#chr`, "-", data$`pos(1-based)`, data$ref, ">", data$alt, "-",
+                              data$aaref, data$aaalt, sep = "")
+all$chr_var_with_aa <- paste(all$chr_vars, "-", all$Ref_AA, all$Alt_AA, sep = "")
+
+common_vars <- intersect(data$chr_var_with_aa, all$chr_var_with_aa)
+
+dbnsfp_allvar <- data[match(common_vars, data$chr_var_with_aa),]
+all_sub <- all[match(common_vars, all$chr_var_with_aa),]
+
+print(sprintf("Number of common variants is %s = %s", sum(dbnsfp_allvar$chr_var_with_aa==all_sub$chr_var_with_aa),
+              length(dbnsfp_allvar$chr_var_with_aa)))
+
+dbnsfp_allvar$prot_vars <- all_sub$prot_vars
+dbnsfp_allvar$variant_info <- all_sub$variant_info
+dbnsfp_allvar$UNIPROTKB <- all_sub$UNIPROTKB
+
+tools <- grep("rankscore", colnames(dbnsfp_allvar))
+tools_considering_AF <- c("MetaSVM_rankscore", "MetaLR_rankscore", "MetaRNN_rankscore",
+                          "BayesDel_addAF_rankscore", "BayesDel_noAF_rankscore", "M-CAP_rankscore",
+                          "MutPred_rankscore", "ClinPred_rankscore", "Eigen-raw_coding_rankscore")
+tools_lessnumvar <- c("LINSIGHT_rankscore")
+
+tools <- tools[-match(tools_considering_AF, colnames(dbnsfp_allvar)[tools])]
+tools <- tools[-match(tools_lessnumvar, colnames(dbnsfp_allvar)[tools])]
+
+dbnsfp_allvar <- dbnsfp_allvar[, c(1:13, tools, 14:37, 786:789)]
+
+save(dbnsfp_allvar, file="dbNSFP_44a_CompleteSet.RData")
+
 load("Test_gnomAD_Shared.RData")
-load("lightgbm_replication_1_prediction.RData")
-test <- cbind(prediction$test_prediction, test)
 
-load("dbNSFP_44a_TestSet.RData")
-dbnsfp <- dbnsfp_testset
+common_vars_test <- intersect(test$prot_vars, dbnsfp_allvar$prot_vars)
+dbnsfp_testset <- dbnsfp_allvar[match(common_vars_test, dbnsfp_allvar$prot_vars),]
 
-s <- colnames(dbnsfp)
-tools <- colnames(dbnsfp)[grep("_rankscore", colnames(dbnsfp))]
-check <- dbnsfp[,match(tools, colnames(dbnsfp))]
-
-dis <- c()
-keep <- c()
-for (i in 1:length(check$SIFT_converted_rankscore)){
-  print(i)
-  vals <- as.numeric(check[i,which(check[i,]!=".")])
-  
-  if (length(vals)>=8){
-    t <- length(vals)
-    thr <- ceiling(t/2)-2
-    l1 <- which(vals>0.5)
-    l2 <- which(vals<0.5)
-    if (length(l1)>=thr && length(l2)>=thr){
-      keep <- c(keep, i)
-      dis <- rbind(dis, c(length(l1), length(l2)))
-    }
-  }
-}
-
-vars <- dbnsfp$prot_vars[keep]
-
-test <- test[match(vars, test$prot_vars),]
-dbnsfp <- dbnsfp[match(vars, dbnsfp$prot_vars),]
-dbnsfp_sub <- check[keep,]
-
-print(sprintf("The variants of test set and dbNSFP matches? %s", length(sum(test$prot_vars==dbnsfp$prot_vars))))
-
-hard_cases <- cbind(test$prot_vars, test$variant_info, test$`prediction$test_prediction`,
-                    dbnsfp_sub)
-colnames(hard_cases)[1:3] <- c("prot_vars", "variant_info", "PHACTboost")
-
-print(table(hard_cases$variant_info))
-
-save(hard_cases, file = "Hard_Cases.RData")
+save(dbnsfp_testset, file="dbNSFP_44a_TestSet.RData")
