18916c98c462daee17ebc76b178d61850c15a7bb
Nurdan Kuru <68369488+nurdannkuru@users.noreply.github.com>
2023-09-09 23:57:19 +0300

    Update ConstructHardCases.R

--- dbNSFP/ConstructHardCases.R
+++ dbNSFP/ConstructHardCases.R
@@ -1,8 +1,8 @@
-load("/Users/nurdankuru/Desktop/PHACTboost/Test_gnomAD_Shared.RData")
-load("/Users/nurdankuru/Desktop/PHACTboost/lightgbm_replication_1_prediction.RData")
+load("Test_gnomAD_Shared.RData")
+load("lightgbm_replication_1_prediction.RData")
 test <- cbind(prediction$test_prediction, test)
 
-load("/Users/nurdankuru/dbNSFP_44a_TestSet.RData")
+load("dbNSFP_44a_TestSet.RData")
 dbnsfp <- dbnsfp_testset
 
 s <- colnames(dbnsfp)
@@ -42,3 +42,28 @@ colnames(hard_cases)[1:3] <- c("prot_vars", "variant_info", "PHACTboost")
 print(table(hard_cases$variant_info))
 
 save(hard_cases, file = "Hard_Cases.RData")
+
+load("Train_gnomAD_Shared.RData")
+load("Test_gnomAD_Shared.RData")
+all_data <- rbind(train, test)
+
+common_prots <- intersect(unique(train$UNIPROTKB), unique(test$UNIPROTKB))
+
+el_ids <- c()
+for (id in common_prots){
+  sub <- all_data[which(all_data$UNIPROTKB==id),]
+  if (length(unique(sub$variant_info))<2){
+    el_ids <- c(el_ids, id)
+  }
+}
+
+el <- c()
+for (id in el_ids){
+  el <- c(el, grep(id, hard_cases$prot_vars))
+}
+
+hard_cases_mixed <- hard_cases[-el, ]
+
+
+save(hard_cases_mixed, file = "Hard_Cases_Mixed.RData")
+
